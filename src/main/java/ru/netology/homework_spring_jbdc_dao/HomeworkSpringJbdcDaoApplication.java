d948760ea1c9cbddb71f10cf2fd2ba1ac2e4f6a8
Ilya Turkovsky <ilcheg@yandex.ru>
2023-06-02 22:38:16 +0300

    dont work

--- src/main/java/ru/netology/homework_spring_jbdc_dao/HomeworkSpringJbdcDaoApplication.java
+++ src/main/java/ru/netology/homework_spring_jbdc_dao/HomeworkSpringJbdcDaoApplication.java
@@ -1,5 +1,6 @@
 package ru.netology.homework_spring_jbdc_dao;
 
+import jakarta.persistence.JoinTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,13 +16,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class HomeworkSpringJbdcDaoApplication {
+    private static NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private static final String myScript = read("myScript.sql");
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HomeworkSpringJbdcDaoApplication.class, args);
+
+        MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource("name", "Alexey");
+        List<Product> products = namedParameterJdbcTemplate.query(myScript, sqlParameterSource, (
+                (rs, rowNum) -> new Product(rs.getString("product_name"))));
+        products.forEach(System.out::println);
+    }
+
+    //            List<Customer> customers = namedParameterJdbcTemplate.query("select name from netology.customers where name = 'Alexey'", (
+//                (rs, rowNum) -> new Customer(rs.getString("name"))));
+//        customers.forEach(System.out::println);
+//    }
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
